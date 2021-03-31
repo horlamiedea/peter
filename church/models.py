4e2ba14ed19e0ee1b272bf1165eadb20a22071f6
horlamiedea <horlamiedea@gmail.com>
2021-03-31 12:19:19 +0100

    front page done

--- church/models.py
+++ church/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField()
+    location = models.CharField(max_length=100)
+    def __str__(self):
+        
+        return self.name
+     
+     
+class New(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField()
+    news = models.TextField(blank=True, null=True)
+    
+    
+class Sermon(models.Model):
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    date = models.DateField()
+    sermon = models.TextField(blank=True, null=True)
+    
+    
+class Contact(models.Model):
+    name = models.CharField(max_length=300)
+    email = models.EmailField()
+    message = models.TextField()
+    
--- church/views.py
+++ church/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+from django.views.generic import View
+from .models import Event, New, Sermon
+from django.core.paginator import Paginator
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+class Home(View):
+    def get(self, *args, **kwargs):
+        sermon = Sermon.objects.all()
+        paginator = Paginator(sermon, 4)
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        event = Event.objects.all()
+        pag = Paginator(event, 4)
+        pag_number = self.request.GET.get('page')
+        pag_obj = paginator.get_page(pag_number)
+        new = New.objects.all()
+        pags = Paginator(new, 4)
+        pags_number = self.request.GET.get('page')
+        pags_obj = pags.get_page(pags_number)
+        context = {
+            'page_obj': page_obj,
+            'pag_obj': pag_obj,
+            'pags_obj': pags_obj
+        }
+        return render(self.request, 'index.html', context)
